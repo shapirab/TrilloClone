14d67db8067dd41437c31b3adb412513210eaea3
shapirab <shapirab@jamd.ac.il>
2023-06-07 07:25:35 +0300

    7/6/23 added a nav-bar empty component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { ContentComponent } from './components/content/content.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavBarComponent,
+    ContentComponent
   ],
   imports: [
     BrowserModule,
