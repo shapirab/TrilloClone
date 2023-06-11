c7c940ccba394a586250e74d5a7662d51ffc0bc7
shapirab <shapirab@jamd.ac.il>
2023-06-11 13:25:08 +0300

    11/6/23 added the student, faculty and courses components. Set the content page and routers.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { StudentsComponent } from './components/students/students.component';
+import { FacultyComponent } from './components/faculty/faculty.component';
+import { CoursesComponent } from './components/courses/courses.component';
 
 const routes: Routes = [
+  {path: 'home/students', component: StudentsComponent},
+  {path: 'home/faculty', component: FacultyComponent},
+  {path: 'home/courses', component: CoursesComponent},
   {path: 'home', component: HomeComponent},
   {path: '', redirectTo: '/home', pathMatch: 'full'}
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,23 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { ContentComponent } from './components/content/content.component';
+import { NavLeftComponent } from './components/nav-left/nav-left.component';
+import { ContentMainComponent } from './components/content-main/content-main.component';
+import { StudentsComponent } from './components/students/students.component';
+import { FacultyComponent } from './components/faculty/faculty.component';
+import { CoursesComponent } from './components/courses/courses.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     NavBarComponent,
-    ContentComponent
+    ContentComponent,
+    NavLeftComponent,
+    ContentMainComponent,
+    StudentsComponent,
+    FacultyComponent,
+    CoursesComponent,
   ],
   imports: [
     BrowserModule,
